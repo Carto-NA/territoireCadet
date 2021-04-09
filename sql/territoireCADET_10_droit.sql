58a929b78fd49f96a44023ee3b843019fce83651
Carto-NA <63336882+Carto-NA@users.noreply.github.com>
2021-04-09 09:11:45 +0200

    Create territoireCADET_10_droit.sql

---
+++ sql/territoireCADET_10_droit.sql
@@ -0,0 +1,15 @@
+/* TERRITOIRE CADET V1.0 */
+/* Creation de la structure des données (tables, séquences, triggers,...) */
+/* territoireCADET_10_droit.sql */
+/* PostgreSQL/PostGIS */
+/* Conseil régional Nouvelle-Aquitaine - https://cartographie.nouvelle-aquitaine.fr/ */
+/* Auteur : Tony VINCENT */
+
+------------------------------------------------------------------------ 
+-- Grant : Droits des éléments
+------------------------------------------------------------------------
+
+-- Tables : met_pat.m_pat_lycee_p_construction
+--GRANT ALL ON TABLE met_pat.m_pat_lycee_p_construction TO "pre-sig-usr";
+--GRANT ALL ON TABLE met_pat.m_pat_lycee_p_construction TO "pre-sig-ro";
+--GRANT ALL ON SEQUENCE met_pat.m_pat_lycee_p_construction_id_seq TO "pre-sig-ro";
