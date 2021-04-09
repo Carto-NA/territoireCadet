0745e51c33265b2a183c1392dde5a32869e1c1a4
Carto-NA <63336882+Carto-NA@users.noreply.github.com>
2021-04-09 09:14:29 +0200

    Update territoireCADET_10_structure.sql

--- sql/territoireCADET_10_structure.sql
+++ sql/territoireCADET_10_structure.sql
@@ -7,9 +7,30 @@
 
 
 ------------------------------------------------------------------------
--- Table: met_pat.m_pat_lycee_p_construction
+-- Table: referentiel.na_territoire_cadet;
 
---DROP TABLE met_pat.m_pat_lycee_p_construction;
+-- DROP TABLE referentiel.na_territoire_cadet;
+CREATE TABLE referentiel.na_territoire_cadet (
+	objectid int4 NOT NULL,
+	code_cadet varchar(20) NULL,
+	nom_cadet varchar(122) NULL,
+	siren_epci varchar(18) NULL,
+	nom_epci varchar(84) NULL
+);
+CREATE UNIQUE INDEX r567_sde_rowid_uk ON referentiel.na_territoire_cadet USING btree (objectid) WITH (fillfactor='75');
+
+------------------------------------------------------------------------
+-- Table: referentiel.na_territoire_cadet;
+
+-- DROP TABLE referentiel.ign_ade_territoire_cadet;
+CREATE TABLE referentiel.ign_ade_territoire_cadet (
+	objectid int4 NOT NULL,
+	code_cadet varchar(20) NULL,
+	arcgis_referentiel_na_territo_1 varchar(122) NULL,
+	shape sde.st_geometry NULL
+);
+CREATE INDEX a465_ix1 ON referentiel.ign_ade_territoire_cadet USING gist (shape);
+CREATE UNIQUE INDEX r636_sde_rowid_uk ON referentiel.ign_ade_territoire_cadet USING btree (objectid) WITH (fillfactor='75');
 
 
 ------------------------------------------------------------------------
